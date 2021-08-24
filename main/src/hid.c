00b6131bb694b86c8faa27eca1dcd0304c1e580e
XiNGRZ <hi@xingrz.me>
2021-08-25 01:58:55 +0800

    Format HID descriptors
    https://eleccelerator.com/usbdescreqparser/

--- main/src/hid.c
+++ main/src/hid.c
@@ -20,77 +20,7 @@
 
 #define COUNT(x) (sizeof(x) / sizeof(x[0]))
 
-const unsigned char hidapiReportMap[] = {
-		// 8 bytes input, 8 bytes feature
-		0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
-		0x0A, 0x00, 0x01,  // Usage (0x0100)
-		0xA1, 0x01,  // Collection (Application)
-		0x85, 0x01,  //   Report ID (1)
-		0x15, 0x00,  //   Logical Minimum (0)
-		0x26, 0xFF, 0x00,  //   Logical Maximum (255)
-		0x75, 0x08,  //   Report Size (8)
-		0x95, 0x08,  //   Report Count (8)
-		0x09, 0x01,  //   Usage (0x01)
-		0x82, 0x02, 0x01,  //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
-		//   Position,Buffered Bytes)
-		0x95, 0x08,  //   Report Count (8)
-		0x09, 0x02,  //   Usage (0x02)
-		0xB2, 0x02, 0x01,  //   Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
-		//   Position,Non-volatile,Buffered Bytes)
-		0x95, 0x08,  //   Report Count (8)
-		0x09, 0x03,  //   Usage (0x03)
-		0x91, 0x02,  //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null
-		//   Position,Non-volatile)
-		0xC0,  // End Collection
-};
-
-const unsigned char keyboardReportMap[] = {
-		0x05, 0x01,  // Usage Pg (Generic Desktop)
-		0x09, 0x06,  // Usage (Keyboard)
-		0xA1, 0x01,  // Collection: (Application)
-		0x85, 0x02,  // Report Id (2)
-		//
-		0x05, 0x07,  //   Usage Pg (Key Codes)
-		0x19, 0xE0,  //   Usage Min (224)
-		0x29, 0xE7,  //   Usage Max (231)
-		0x15, 0x00,  //   Log Min (0)
-		0x25, 0x01,  //   Log Max (1)
-		//
-		//   Modifier byte
-		0x75, 0x01,  //   Report Size (1)
-		0x95, 0x08,  //   Report Count (8)
-		0x81, 0x02,  //   Input: (Data, Variable, Absolute)
-		//
-		//   Reserved byte
-		0x95, 0x01,  //   Report Count (1)
-		0x75, 0x08,  //   Report Size (8)
-		0x81, 0x01,  //   Input: (Constant)
-		//
-		//   LED report
-		0x95, 0x05,  //   Report Count (5)
-		0x75, 0x01,  //   Report Size (1)
-		0x05, 0x08,  //   Usage Pg (LEDs)
-		0x19, 0x01,  //   Usage Min (1)
-		0x29, 0x05,  //   Usage Max (5)
-		0x91, 0x02,  //   Output: (Data, Variable, Absolute)
-		//
-		//   LED report padding
-		0x95, 0x01,  //   Report Count (1)
-		0x75, 0x03,  //   Report Size (3)
-		0x91, 0x01,  //   Output: (Constant)
-		//
-		//   Key arrays (6 bytes)
-		0x95, 0x06,  //   Report Count (6)
-		0x75, 0x08,  //   Report Size (8)
-		0x15, 0x00,  //   Log Min (0)
-		0x25, 0x65,  //   Log Max (101)
-		0x05, 0x07,  //   Usage Pg (Key Codes)
-		0x19, 0x00,  //   Usage Min (0)
-		0x29, 0x65,  //   Usage Max (101)
-		0x81, 0x00,  //   Input: (Data, Array)
-		//
-		0xC0,  // End Collection
-};
+#include "hid_descriptors.h"
 
 static esp_hid_raw_report_map_t report_maps[] = {
 		{.data = hidapiReportMap, .len = sizeof(hidapiReportMap)},
