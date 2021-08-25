f3ee53eda1405b65b6c1419a5c07532de845d381
XiNGRZ <hi@xingrz.me>
2021-08-25 15:04:29 +0800

    Clean up hidapi report map

--- main/src/hid_descriptors.h
+++ main/src/hid_descriptors.h
@@ -1,23 +1,12 @@
 const unsigned char hidapiReportMap[] = {
     0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
-    0x0A, 0x00, 0x01,  // Usage (0x0100)
+    0x09, 0x01,        // Usage (0x01)
     0xA1, 0x01,        // Collection (Application)
     0x85, 0x01,        //   Report ID (1)
 
-    0x15, 0x00,        //   Logical Minimum (0)
-    0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+    0x95, 0x01,        //   Report Count (1)
     0x75, 0x08,        //   Report Size (8)
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x01,        //   Usage (0x01)
-    0x82, 0x02, 0x01,  //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Buffered Bytes)
-
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x02,        //   Usage (0x02)
-    0xB2, 0x02, 0x01,  //   Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile,Buffered Bytes)
-
-    0x95, 0x08,        //   Report Count (8)
-    0x09, 0x03,        //   Usage (0x03)
-    0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
 
     0xC0,              // End Collection
 };
